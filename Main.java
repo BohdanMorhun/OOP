f189719d62bdce9233731ff6565e2e28b5efcdca
BohdanMorhun <44846410+BohdanMorhun@users.noreply.github.com>
2018-11-13 13:50:44 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,11 +1,19 @@
 package com.company;
 
+
 public class Main {
     public static void main(String[] args) {
-        Triangle tr = new Triangle(5, 7, 8, 6, 9);
+        Point a = new Point(2,3);
+        Point b = new Point(3,5);
+        Point c = new Point(6,9);
+        Point d = new Point(4,8);
+        Triangle tr = new Triangle(a,b,c);
         System.out.println(tr.calculateArea());
-        Rectangle r = new Rectangle(4, 5, 7, 8, 5, 5);
+        Rectangle r = new Rectangle(a,b,c,d);
+        System.out.println(r.calculateArea());
         System.out.println(r.calculatePerimetr());
+
 
     }
 }
+
